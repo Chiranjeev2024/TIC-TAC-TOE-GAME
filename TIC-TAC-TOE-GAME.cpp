5a9a378eedae80f5fdab7f7c98b3b9f6957b8c9a
Chiranjeev2024 <sks7369064258@gmail.com>
2024-05-14 16:27:53 +0530

    function3 initializing

--- TIC-TAC-TOE-GAME.cpp
+++ TIC-TAC-TOE-GAME.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int matrix[3][3] = {{1,2,3}, {4,5,6},{7,8,9}};
 int row;
-int comumn;
+int column;
 char token;
+string n1;
+string n2;
 
 void function1(){
 
-    string n1;
-    string n2;
+    
 
     cout<<"Enter the name of first player: "<<endl;
     getline(cin, n1);
@@ -31,6 +32,91 @@ void function1(){
 }
 
 void function2(){
+
+    int digit;
+    
+    if(token == 'x'){
+        cout<<n1<<" please enter";
+        cin>>digit;
+    }
+
+    if(token == '0'){
+        cout<<n2<<" please enter";
+        cin>>digit;
+    }
+
+
+    if(digit == 1){
+        row = 0;
+        column = 0;
+
+    }
+    if(digit == 2){
+        row = 0;
+        column = 1;
+        
+    }
+    if(digit == 3){
+        row = 0;
+        column = 2;
+        
+    }
+    if(digit == 4){
+        row = 1;
+        column = 0;
+        
+    }
+    if(digit == 5){
+        row = 1;
+        column = 1;
+        
+    }
+    if(digit == 6){
+        row = 1;
+        column = 2;
+        
+    }
+    if(digit == 7){
+        row = 2;
+        column = 0;
+        
+    }
+    if(digit == 8){
+        row = 2;
+        column = 1;
+        
+    }
+    if(digit == 9){
+        row = 2;
+        column = 2;
+        
+    }
+    else{
+        cout<<"Invalid !!!"<<endl;
+    }
+
+    
+
+    if(token == 'x' && matrix[row][column] != 'x' && matrix[row][column] != '0'){
+
+        matrix[row][column] = 'x';
+        token = '0';
+    }
+    else if(token == '0' && matrix[row][column] != 'x' && matrix[row][column] != '0'){
+
+        matrix[row][column] = '0';
+        token = 'x';
+    }
+    else
+    {
+        cout<<"There is no empty space !"<<endl;
+        function2();
+    }
+    
+    function1();
+}
+
+bool function3(){
 
     
 }
