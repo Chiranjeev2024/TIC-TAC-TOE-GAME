e896f887e03de204994c7cd8b5ba40c96a11c2e3
Chiranjeev2024 <sks7369064258@gmail.com>
2024-05-14 16:02:28 +0530

    initializing function2

--- TIC-TAC-TOE-GAME.cpp
+++ TIC-TAC-TOE-GAME.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int matrix[3][3] = {{1,2,3}, {4,5,6},{7,8,9}};
 int row;
 int comumn;
-
+char token;
 
 void function1(){
 
@@ -30,3 +30,7 @@ void function1(){
     cout<<"     |     |   "<<endl;
 }
 
+void function2(){
+
+    
+}
