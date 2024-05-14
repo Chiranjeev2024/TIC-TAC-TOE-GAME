e2dba864b36aebbabeece7d6753cf7b3ec6884c0
Chiranjeev2024 <sks7369064258@gmail.com>
2024-05-14 15:52:38 +0530

    tic-tac-toe board design

---
+++ TIC-TAC-TOE-GAME.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int matrix[3][3] = {{1,2,3}, {4,5,6},{7,8,9}};
+
+    string n1;
+    string n2;
+
+    cout<<"Enter the name of first player: "<<endl;
+    // getline(cin, n1);
+    // cout<<"Enter the name of second player: "<<endl;
+    // getline(cin, n2);
+
+    cout<<n1<<" is the player1 so he/she play first"<<endl;
+    cout<<n2<<" is the player1 so he/she play second"<<endl;
+
+    cout<<"     |     |   "<<endl;
+    cout<<"  "<<matrix[0][0]<<"  |  "<<matrix[0][1]<<"  |  "<<matrix[0][2]<<"  "<<endl;
+    cout<<"_____|_____|_____"<<endl;
+    cout<<"     |     |   "<<endl;
+    cout<<"  "<<matrix[1][0]<<"  |  "<<matrix[1][1]<<"  |  "<<matrix[1][2]<<"  "<<endl;
+    cout<<"_____|_____|_____"<<endl;
+    cout<<"     |     |   "<<endl;
+    cout<<"  "<<matrix[2][0]<<"  |  "<<matrix[2][1]<<"  |  "<<matrix[2][2]<<"  "<<endl;
+    cout<<"     |     |   "<<endl;
+}
